01d1cd6250f9963448c53d01ed2efaf396409092
comShadoHarvy <jimbob343@gmail.com>
2025-02-24 16:44:25 -0500

    Remove unnecessary tsserver from Mason configuration and update Mini.nvim setup

--- .config/nvim/lua/shadowharvy/plugins/mini.lua
+++ .config/nvim/lua/shadowharvy/plugins/mini.lua
@@ -2,9 +2,6 @@ return {
     "echasnovski/mini.nvim",
     version = false,  -- Use 'false' to always get the latest commit, or pin a specific commit/tag
     config = function()
-      require("mini").setup({
-        -- Add your custom options here if needed
-      })
+      -- Removed require("mini").setup() as mini.nvim does not have a global setup function.
     end,
   }
-  
