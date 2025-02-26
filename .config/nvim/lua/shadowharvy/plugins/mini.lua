494073fca6a92072027d4366e73f78ea7c7927fb
comShadoHarvy <jimbob343@gmail.com>
2025-02-26 18:49:24 -0500

    Refactor Mini.nvim configuration to simplify module setup and remove error handling

--- .config/nvim/lua/shadowharvy/plugins/mini.lua
+++ .config/nvim/lua/shadowharvy/plugins/mini.lua
@@ -1,117 +1,31 @@
 return {
-    "echasnovski/mini.nvim",
-    version = false, -- Use 'false' to always get the latest commit, or pin a specific commit/tag
-    config = function()
-        -- Setup each module individually without requiring 'mini' directly.
-
-        -- Modules with setup function
-        local status_ok, animate = pcall(require, "mini.animate")
-        if status_ok then
-            animate.setup()
-        end
-
-        local status_ok, basics = pcall(require, "mini.basics")
-        if status_ok then
-            basics.setup()
-        end
-
-        local status_ok, bracketed = pcall(require, "mini.bracketed")
-        if status_ok then
-            bracketed.setup()
-        end
-        
-        local status_ok, clue = pcall(require, "mini.clue")
-        if status_ok then
-            clue.setup()
-        end
-
-        local status_ok, colors = pcall(require, "mini.colors")
-        if status_ok then
-            colors.setup()
-        end
-
-        local status_ok, comment = pcall(require, "mini.comment")
-        if status_ok then
-            comment.setup()
-        end
-
-        local status_ok, cursorword = pcall(require, "mini.cursorword")
-        if status_ok then
-            cursorword.setup()
-        end
-
-        local status_ok, files = pcall(require, "mini.files")
-        if status_ok then
-            files.setup()
-        end
-
-        local status_ok, jump = pcall(require, "mini.jump")
-        if status_ok then
-            jump.setup()
-        end
-
-        local status_ok, map = pcall(require, "mini.map")
-        if status_ok then
-            map.setup()
-        end
-
-        local status_ok, move = pcall(require, "mini.move")
-        if status_ok then
-            move.setup()
-        end
-
-        local status_ok, operators = pcall(require, "mini.operators")
-        if status_ok then
-            operators.setup()
-        end
-
-        local status_ok, pairs = pcall(require, "mini.pairs")
-        if status_ok then
-            pairs.setup()
-        end
-
-        local status_ok, pick = pcall(require, "mini.pick")
-        if status_ok then
-            pick.setup()
-        end
-
-        local status_ok, sessions = pcall(require, "mini.sessions")
-        if status_ok then
-            sessions.setup()
-        end
-
-        local status_ok, surround = pcall(require, "mini.surround")
-        if status_ok then
-            surround.setup()
-        end
-
-        local status_ok, tabline = pcall(require, "mini.tabline")
-        if status_ok then
-            tabline.setup()
-        end
-
-        local status_ok, trailspace = pcall(require, "mini.trailspace")
-        if status_ok then
-            trailspace.setup()
-        end
-        
-        local status_ok, ai = pcall(require, "mini.ai")
-        if status_ok then
-            ai.setup()
-        end
-
-        -- Modules without a setup function:
-        -- You would access these directly as needed.
-        -- Example:
-        -- local status_ok, misc = pcall(require, "mini.misc")
-        -- if status_ok then
-        --     -- use misc.is_string, or other exported functions.
-        --     if misc.is_string("hello") then
-        --         print("It's a string!")
-        --     end
-        -- end
-        -- You can also add these in the same pattern, like above.
-        -- This is for all the other modules without a setup() function.
-        print("All Mini modules loaded!")
-    end,
+  "echasnovski/mini.nvim",
+  version = false, -- Use 'false' to always get the latest commit
+  config = function()
+    -- Remove this line, as it will cause an error:
+    -- local mini = require("mini")
+    
+    -- Set up modules that have a setup function
+    require("mini.animate").setup()
+    require("mini.basics").setup()
+    require("mini.bracketed").setup()
+    require("mini.clue").setup()
+    require("mini.colors").setup()
+    require("mini.comment").setup()
+    require("mini.cursorword").setup()
+    require("mini.files").setup()
+    require("mini.jump").setup()
+    require("mini.map").setup()
+    require("mini.move").setup()
+    require("mini.operators").setup()
+    require("mini.pairs").setup()
+    require("mini.pick").setup()
+    require("mini.sessions").setup()
+    require("mini.surround").setup()
+    require("mini.tabline").setup()
+    require("mini.trailspace").setup()
+    require("mini.ai").setup()
+    
+    print("All Mini modules loaded!")
+  end,
 }
