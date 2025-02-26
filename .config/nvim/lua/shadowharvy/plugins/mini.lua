28550d2b57b7d81e975f0988bcb987819fda8dae
comShadoHarvy <jimbob343@gmail.com>
2025-02-26 18:03:03 -0500

    Setup additional Mini.nvim modules for enhanced functionality

--- .config/nvim/lua/shadowharvy/plugins/mini.lua
+++ .config/nvim/lua/shadowharvy/plugins/mini.lua
@@ -3,5 +3,9 @@ return {
     version = false,  -- Use 'false' to always get the latest commit, or pin a specific commit/tag
     config = function()
       -- Removed require("mini").setup() as mini.nvim does not have a global setup function.
+      require("mini.cursorword").setup()
+      require("mini.surround").setup()
+      require("mini.comment").setup()
+      -- Add additional modules as needed.
     end,
   }
